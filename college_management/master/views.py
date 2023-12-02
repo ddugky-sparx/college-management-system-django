fe769c673261d981f1b3e0e80337a7668b80a0dc
ddugky-sparx <batch02.ddugky@gmail.com>
2023-12-02 13:00:46 +0530

    read aND CREATED IN SQL

--- college_management/master/views.py
+++ college_management/master/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from master.models import Qualification
 # Create your views here.
 def designation_data(request):
     return render(request,'designation_data.html',{"name":"shameel"})
@@ -11,15 +11,22 @@ def designation_add(request):
     return render(request,'designation_add.html')
 
 def qualification_data(request):
-    return render(request,'qualification_data.html')
+    data=Qualification.objects.all()
+    return render(request,'qualification_data.html',{"data":data})
 
 def qualification_edit(request):
     return render(request,'qualification_edit.html')
 
 def qualification_add(request):
+
     if request.POST:
         qualification=request.POST['qualification']
-    return render(request,'qualification_add.html')
+        instance=Qualification(name=qualification)
+        instance.save()
+        message="Submitted"
+        return render(request,'qualification_add.html',{"message":message})  
+    else:
+        return render(request,'qualification_add.html')
 
         
 
