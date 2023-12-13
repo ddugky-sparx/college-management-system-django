a115d72c4455dc4aa7070ef8593dd4f1e86bdb74
ddugky-sparx <batch02.ddugky@gmail.com>
2023-12-13 13:05:35 +0530

    checked change password

--- college_management/AdminPanel/urls.py
+++ college_management/AdminPanel/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.urls import path,include
+from django.contrib.auth.views import PasswordChangeView
 from . import views
 
 urlpatterns = [
    path('Admin-login/',views.adminpanel),
    path('Admin-change/',views.change_password,name="change_password"),
+   path('accounts/password_change/', PasswordChangeView.as_view(template_name='change_password.html'), name='password_change'),
    path("accounts/", include("django.contrib.auth.urls")),
+   
+   
    
 ]
